677129182371505abe91f30eed3139adae2fca59
strin <strin@DESKTOP-BFUD0BJ>
2023-06-07 18:53:47 +0530

    new

---
+++ Mahi/src/test/java/com/Mahi/testscripts/CreateCustomer.java
@@ -0,0 +1,12 @@
+package com.Mahi.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	@Test
+	public void testCreateCustomer() {
+		Reporter.log("GoodEvening",true);
+	}
+
+}
